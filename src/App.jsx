9f61b0f3b14c30f616ac7011f987a793eee11156
belen <bel.f_91@hotmail.com>
2025-12-03 16:42:05 -0300

    NavegaLasRutas+Fernandez

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter basename="/E-commerce-Fernandez">
+    <HashRouter basename="/E-commerce-Fernandez">
       <NavBar />
 
       <Routes>
