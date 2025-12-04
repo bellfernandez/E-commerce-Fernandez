ddaab4170149e00c5a56719969c4fc796672ba97
belen <bel.f_91@hotmail.com>
2025-12-04 09:31:23 -0300

    NavegaTusRutas+Fernandez

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./index"; // NavBar import
+import NavBar from "./components/layout/NavBar"; 
 import ItemListContainer from "./components/container/ItemListContainer";
 import ItemDetailContainer from "./components/container/ItemDetailContainer";
 import "./App.css";
 
 function App() {
   return (
-    <HashRouter basename="/E-commerce-Fernandez">
+    <HashRouter>
       <NavBar />
+
       <Routes>
-        <Route path="/" element={<ItemListContainer greeting="¡Bienvenida a tu tienda online, Belén!" />} />
-        <Route path="/category/:categoryId" element={<ItemListContainer />} />
-        <Route path="/item/:id" element={<ItemDetailContainer />} />
-        <Route path="*" element={<h2 style={{ textAlign: "center", marginTop: "3rem" }}>404 - Página no encontrada</h2>} />
+        <Route 
+          path="/" 
+          element={<ItemListContainer greeting="¡Bienvenida a tu tienda online, Belén!" />} 
+        />
+
+        <Route 
+          path="/category/:categoryId" 
+          element={<ItemListContainer />} 
+        />
+
+        <Route 
+          path="/item/:id" 
+          element={<ItemDetailContainer />} 
+        />
+
+        <Route 
+          path="*" 
+          element={
+            <h2 style={{ textAlign: "center", marginTop: "3rem" }}>
+              404 - Página no encontrada
+            </h2>
+          } 
+        />
       </Routes>
+
     </HashRouter>
   );
 }
